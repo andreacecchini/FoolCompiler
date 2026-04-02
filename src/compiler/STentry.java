bc81ef61a098be495b603d65b1128ad14fc722c0
Andrea Cecchini <andrea.cecchini13@studio.unibo.it>
2026-04-02 18:48:53 +0200

    STEntry from virtuale

--- src/compiler/STentry.java
+++ src/compiler/STentry.java
@@ -3,18 +3,13 @@ package compiler;
 import compiler.lib.*;
 
 public class STentry implements Visitable {
-    int nl;
-    TypeNode type;
-    int offset;
-
-    public STentry(int n, TypeNode t, int o) {
-        nl = n;
-        type = t;
-        offset = o;
-    }
+    final int nl;
+    final TypeNode type;
+    final int offset;
+    public STentry(int n, TypeNode t, int o) { nl = n; type = t; offset=o; }
 
     @Override
-    public <S, E extends Exception> S accept(BaseASTVisitor<S, E> visitor) throws E {
-        return ((BaseEASTVisitor<S, E>) visitor).visitSTentry(this);
+    public <S,E extends Exception> S accept(BaseASTVisitor<S,E> visitor) throws E {
+        return ((BaseEASTVisitor<S,E>) visitor).visitSTentry(this);
     }
 }
