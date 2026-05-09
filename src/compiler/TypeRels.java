ee5c593ee4b51a8039b79beb61df147d287ddcc6
Andrea Cecchini <andrea.cecchini13@studio.unibo.it>
2026-05-09 19:24:58 +0200

    refactor: improving TypeRels

--- src/compiler/TypeRels.java
+++ src/compiler/TypeRels.java
@@ -7,27 +7,36 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.IntStream;
 
 public class TypeRels {
+    /**
+     * Maps each class ID to its direct superclass ID.
+     * Populated during semantic analysis when class declarations are processed.
+     */
     public static final Map<String, String> SUPER_TYPE = new HashMap<>();
 
     public static boolean isSubtype(TypeNode a, TypeNode b) {
-        // Checking method.
+        // Checking methods (on overrides).
         if (a instanceof ArrowTypeNode aFun) {
-            return b instanceof ArrowTypeNode bFun && isSubTypeFun(aFun, bFun);
+            return b instanceof ArrowTypeNode bFun && isFunSubType(aFun, bFun);
         }
+        // Checking references.
         if (a instanceof RefTypeNode aRef) {
-            return b instanceof RefTypeNode bRef && isSubTypeRel(aRef, bRef);
+            return b instanceof RefTypeNode bRef && isRelSubType(aRef, bRef);
         }
+        // Checking primitives
         return (a.getClass().equals(b.getClass()))
                 || ((a instanceof BoolTypeNode) && (b instanceof IntTypeNode))
                 || ((a instanceof EmptyTypeNode) && (b instanceof RefTypeNode));
     }
 
-    private static boolean isSubTypeRel(RefTypeNode a, RefTypeNode b) {
+    private static boolean isRelSubType(RefTypeNode a, RefTypeNode b) {
+        // Same class
         if (a.id.equals(b.id)) {
             return true;
         }
+        // Checking a is subtype of b
         String current = SUPER_TYPE.get(a.id);
         final Set<String> visited = new HashSet<>();
         while (current != null && visited.add(current)) {
@@ -39,15 +48,15 @@ public class TypeRels {
         return false;
     }
 
-    private static boolean isSubTypeFun(ArrowTypeNode a, ArrowTypeNode b) {
-        // co-variance on return type / contro-variance on parameters
-        boolean parameters = true;
-        if (!(a.parlist.size() == b.parlist.size())) {
+    private static boolean isFunSubType(ArrowTypeNode a, ArrowTypeNode b) {
+        if (a.parlist.size() != b.parlist.size()) {
             return false;
         }
-        for (int i = 0; i < a.parlist.size(); i++) {
-            parameters = parameters && isSubtype(b.parlist.get(i), a.parlist.get(i));
-        }
-        return isSubtype(a.ret, b.ret) && parameters;
+        // co-variance on return type
+        boolean retType = isSubtype(a.ret, b.ret);
+        // contro-variance on parameters.
+        boolean parameters = IntStream.range(0, a.parlist.size())
+                .allMatch(i -> isSubtype(b.parlist.get(i), a.parlist.get(i)));
+        return retType && parameters;
     }
 }
